8c6a665a9d2a6fe9a36d95fa2da403e0a5d93069
no8ody <no8ody@tanglebay.com>
2020-06-16 18:47:56 +0200

    -

--- auto-swarm.sh
+++ auto-swarm.sh
@@ -2,7 +2,7 @@
 source /etc/hlm-cfgs/swarm.cfg
 source /etc/hlm-cfgs/nginx.cfg
 if [ -f "/etc/hlm-cfgs/swarm.cfg" ] && [ -f "source /etc/hlm-cfgs/nginx.cfg" ]; then
-    if [ "$domain" != "myhornetnode.ddns.net" ]; then
+    if [ "$domain" != "myhornetnode.ddns.net" ] && [ "$password" != "" ]; then
         curl --silent --output /dev/null -X DELETE https://register.tanglebay.org/$nodepassword
         curl -X POST "https://register.tanglebay.org" -H  "accept: */*" -H  "Content-Type: application/json" -d "{ \"name\": \"$nodename\", \"url\": \"https://$domain:$nodeport/api\", \"address\": \"$donationaddress\", \"pow\": \"$pownode\", \"password\": \"$nodepassword\" }" |jq
     fi
